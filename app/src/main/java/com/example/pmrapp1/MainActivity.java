5056796e96a54a95d583141a97f93c3ba12fc513
tb59 <thomas.bourdeaudhuy@gmail.com>
2021-04-19 15:49:03 +0200

    traces d'exécution

--- app/src/main/java/com/example/pmrapp1/MainActivity.java
+++ app/src/main/java/com/example/pmrapp1/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.pmrapp1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String CAT = "EDPMR";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(CAT,"onCreate"); // trace d'exécution
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(CAT,"onStart"); // trace d'exécution
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(CAT,"onResume"); // trace d'exécution
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(CAT,"onRestart"); // trace d'exécution
+    }
+
+    // TODO : créer les 3 autres méthodes de (re)démarrage de l'activité
+    // Cf. cycle de vie des activités dans le poly
+    // Associer à chacune une trace d'exécution
+    // TODO 2 : mettre des points d'arrêt dans chaque méthode
+    // ET exécuter un mode débogage, pas à pas
 }
