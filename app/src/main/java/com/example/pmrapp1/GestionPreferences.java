ddf2fe7481b56432590baefbd1aa5114ab3430a0
tb59 <thomas.bourdeaudhuy@gmail.com>
2021-04-22 10:12:21 +0200

    modif GestionPreferences pour vider login lorsque la case est décochée

--- app/src/main/java/com/example/pmrapp1/GestionPreferences.java
+++ app/src/main/java/com/example/pmrapp1/GestionPreferences.java
@@ -1,18 +1,41 @@
 package com.example.pmrapp1;
 
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
-public class GestionPreferences extends PreferenceActivity {
-
+public class GestionPreferences extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+    CheckBoxPreference cbp;
+    EditTextPreference edtpl;
+    @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //noinspection deprecation
+
         addPreferencesFromResource(R.xml.preferences);
+        cbp = (CheckBoxPreference) findPreference("remember");
+        edtpl = (EditTextPreference) findPreference("login");
+        cbp.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Toast t = Toast.makeText(this,
+                "click cb :" +newValue.toString()
+                        + " pref manipulée : " + preference.getKey(),Toast.LENGTH_SHORT);
+        t.show();
+        if (newValue.equals(false)) {
+            edtpl.setText("");
+        }
+        return true;
     }
 
     // TODO : comment faire pour vider les préférences lorsque l'on
     // décoche la case depuis cette activité ?
+
+
 }
